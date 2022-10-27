e2fd87f480296c672e6152fdea21b79156e3d78f
muzixuan <1628208948@qq.com>
2022-10-27 14:36:25 +0800

    测试协程

--- coroutinedemo/src/main/java/com/demo/coroutine/MainActivity.kt
+++ coroutinedemo/src/main/java/com/demo/coroutine/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.demo.coroutine
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 private const val TAG = "MainActivity"
 
@@ -11,7 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        test1()
+        lifecycleScope.launch {
+            loadData().collect() {
+                Log.d(TAG, "onCreate: flow $it")
+            }
+        }
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_foreground)
     }
 
     private fun test1() {
@@ -19,11 +27,26 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "onCreate:test1 ${Thread.currentThread().name}")
         }
     }
+
     fun test2() {
-        val job=Job()
+        val job = Job()
         CoroutineScope(job).launch {
-
         }
-
     }
+
+    private fun loadData() = flow {
+        Log.d(TAG, "loadData: inter loadData method,run in ${Thread.currentThread().name}")
+        for (i in 1..20) {
+            delay(1000)
+            emit(i)
+        }
+    }.flowOn(Dispatchers.Main)
+        .filter {
+            Log.d(TAG, "loadData:filter in ${Thread.currentThread().name} ")
+            it % 2 == 0
+        }.flowOn(Dispatchers.Default)
+        .map {
+            Log.d(TAG, "loadData:map in ${Thread.currentThread().name} ")
+            it * 10
+        }.flowOn(Dispatchers.IO)
 }
